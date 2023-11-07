2ad201b64628ee8c14430083c2feb045d87c65f4
Adrian Used <adrian@atbim.es>
2023-11-07 19:16:35 +0100

    nombre de la entrega de mi trabajo al repo

---
+++ borrar.js
@@ -0,0 +1,1 @@
+// este fichero lo vamos a borrar
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ let app = express()
 app.use(express.static('wwwroot'))
 app.use(require('./routes/auth.js'))
 app.use(require('./routes/models.js'))
+console.log('modificación ')
 app.listen(PORT, function () {
   console.log(`Server listening on port ${PORT}...`)
 })
